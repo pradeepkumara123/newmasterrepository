9620da43c392bc5d34276008de87bc040902071e
Pradeepkumarav <pradeep7725@gmail.com>
2021-07-16 13:37:39 +0530

    first commit

---
+++ src/com/userlogin/loginpage.java
@@ -0,0 +1,19 @@
+package com.userlogin;
+
+public class loginpage {
+
+	public static void main(String[] args) {
+		
+		int a;
+		int b;
+		int c;
+		
+		a=5;
+		b=6;
+		c=a+b;
+		
+		System.out.println(c);
+		
+		}
+
+}
